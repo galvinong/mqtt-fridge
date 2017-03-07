5a3c791b57d9fac60c63ad8d030e170e51e5a812
galvinong <go1g14@soton.ac.uk>
2017-03-07 12:38:12 +0000

    Fix syntax error in pusher

--- pusher.js
+++ pusher.js
@@ -5,13 +5,13 @@ Allow web-socket connections to subscribe and publish to MQTT topics
 
 var sys = require('sys');
 var net = require('net');
-var mqtt = =require('mqtt');
+var mqtt = require('mqtt');
 
 // create a socket object that listens on port 5000
 var io = require('socket.io').listen(5000);
 
 // create a mqtt client object and connect to the mqtt broker
-var client = new mqtt.MQTTClient(1883, '192.168.1.107', 'pusher');
+var client = new mqtt.MQTTClient(1883, '127.0.0.1', 'pusher');
 
 io.sockets.on('connection', function (socket) {
     // socket conneciton indicates what mqtt topic to subscribe to in
