a11234ff9bdabecf22bf83efac9032d112b3d8dd
galvinong <go1g14@soton.ac.uk>
2017-03-07 04:41:43 +0000

    Refactored node_modules, and fixed iotdemo.html

--- mqtt_test.js
+++ mqtt_test.js
@@ -1,13 +1,13 @@
 var mqtt = require('mqtt');
 var client = mqtt.connect('mqtt:127.0.0.1')
 
-client.on('connect', function() {
+client.on('connect', function () {
     client.subscribe('presence');
     client.publish('presence', 'RF24SN/in/1/1')
 });
 
 client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(message.toString());
-  client.end();
+    // message is Buffer
+    console.log(message.toString());
+    client.end();
 });
--- pusher.js
+++ pusher.js
@@ -5,33 +5,33 @@ Allow web-socket connections to subscribe and publish to MQTT topics
 
 var sys = require('sys');
 var net = require('net');
-var mqtt= = require('mqtt');
+var mqtt = =require('mqtt');
 
 // create a socket object that listens on port 5000
 var io = require('socket.io').listen(5000);
 
 // create a mqtt client object and connect to the mqtt broker
-var client = mqtt.connect('mqtt://127.0.0.1');
+var client = new mqtt.MQTTClient(1883, '127.0.0.1', 'pusher');
 
-io.sockets.on('connection', function(socket) {
-  // socket conneciton indicates what mqtt topic to subscribe to in
-  socket.on('subscribe', function(data) {
-    console.log('Subscribing to: ' + data.topic);
-    socket.join(data.topic);
-    client.subscribe(data.topic);
-  });
+io.sockets.on('connection', function (socket) {
+    // socket conneciton indicates what mqtt topic to subscribe to in
+    socket.on('subscribe', function (data) {
+        console.log('Subscribing to: ' + data.topic);
+        socket.join(data.topic);
+        client.subscribe(data.topic);
+    });
 
-  socket.on('publish', function(data) {
-    console.log('Publishing to: ' + data.topic);
-    client.publish(data.topic.data.payload);
-  });
+    socket.on('publish', function (data) {
+        console.log('Publishing to: ' + data.topic);
+        client.publish(data.topic.data.payload);
+    });
 });
 
 // listen to messages coming from the mqtt broker
-client.on('message', function (topic, payload, packet) {
-  console.log(topic + "=" + payload);
-  io.sockets.emit('mqtt', {
-    'topic': String(topic),
-    'payload':String(payload)
-  });
+client.addListener('mqttData', function (topic, payload) {
+    console.log(topic + "=" + payload);
+    io.sockets.emit('mqtt', {
+        'topic': String(topic),
+        'payload': String(payload)
+    });
 });
