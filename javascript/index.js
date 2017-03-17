27af52c5b14af145834c3896171bd10775a4ef2c
galvinong <go1g14@soton.ac.uk>
2017-03-17 10:42:27 +0000

    Added mongodb code``

--- javascript/index.js
+++ javascript/index.js
@@ -1,50 +1,112 @@
 // Nodejs web server init file, serves html and images
-var http = require("http"),
-    url = require("url"),
-    path = require("path"),
-    fs = require("fs"),
-    port = process.env.PORT || 8080;
+let http = require('http')
+let url = require('url')
+let path = require('path')
+let fs = require('fs')
+let mqtt = require('mqtt')
+// let mongodb = require('mongodb')
+let mongoose = require('mongoose')
+const port = process.env.PORT || 8080
+const mongoport = process.env.PORT || 5000
 
+// For mongodb connection
+const mongodbURI = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://***REMOVED***'
+const deviceRoot = 'RF24SN/in/1/'
+// mqtt client create to subscribe
+let client
+
+mongoose.connect(mongodbURI, function(err, res) {
+	if (err) {
+		console.log('ERROR connecting to ' + mongodbURI + '. ' + err )
+	} else {
+		console.log('Success connected to: ' + uriString)
+
+		// Create a mqtt client and connect it
+		// TODO: probably need to add the username and password
+		client = mqtt.createClient(mongoport, localhost)
+
+		// Subscribes to all sub channel under deviceroot
+		client.subscribe(deviceRoot + '+')
+
+		// Calls function insertEvent when message arrives
+		client.on('message', insertEvent)
+	}
+})
+
+// Schema for the sensor input using mongoose
+const sensorSchema = new mongoose.Schema({
+	sensor: String,
+	events: {
+		value: payload,
+		created: Date,
+	},
+})
+
+// Creates a model based off the schema
+let SensorInput = mongoose.model('RF24SN', sensorSchema)
+
+/**
+ * Handles message arrvied from broker and add to mongodb
+ * @param {string} topic topic that the client is subscribed to
+ * @param {string} payload payload that the topic has, sent periodically
+ */
+function insertEvent(topic, payload) {
+	let key = topic.replace(deviceRoot, '')
+	let sensor = new SensorInput({
+		sensor: key,
+		events: {
+			value: payload,
+			created: new Date(),
+		},
+	})
+	sensor.save(function(err) {
+		if (err) {
+			console.log('Error saving to mongodb')
+		}
+	})
+}
+
+// HTTP Page serving code
 http.createServer(function(request, response) {
-    var uri = url.parse(request.url).pathname,
-        filename = path.join(process.cwd(), uri);
+	let uri = url.parse(request.url).pathname
+	let filename = path.join(process.cwd(), uri)
 
-    var contentTypesByExtension = {
-        '.html': "text/html",
-        '.css': "text/css",
-        '.js': "text/javascript",
-        '.png': "image/png",
-        '.jpg': "image/jpg"
-    };
+	let contentTypesByExtension = {
+		'.html': 'text/html',
+		'.css': 'text/css',
+		'.js': 'text/javascript',
+		'.png': 'image/png',
+		'.jpg': 'image/jpg',
+	}
 
-    fs.exists(filename, function (exists) {
-        if (!exists) {
-            response.writeHead(404, {"Content-Type": "text/plain"});
-            response.write("404 Not Found\n");
-            response.end();
-            return;
-        }
+	fs.exists(filename, function(exists) {
+		if (!exists) {
+			response.writeHead(404, {'Content-Type': 'text/plain'})
+			response.write('404 Not Found\n')
+			response.end()
+			return
+		}
 
-        if (fs.statSync(filename).isDirectory())
-            filename += '/index.html';
+		if (fs.statSync(filename).isDirectory())
+			filename += '/index.html'
 
-        fs.readFile(filename, "binary", function (err, file) {
-            if (err) {
-                response.writeHead(500, {"Content-Type": "text/plain"});
-                response.write(err + "\n");
-                response.end();
-                return;
-            }
+		fs.readFile(filename, 'binary', function(err, file) {
+			if (err) {
+				response.writeHead(500, {'Content-Type': 'text/plain'})
+				response.write(err + '\n')
+				response.end()
+				return
+			}
 
-            var headers = {};
-            var contentType = contentTypesByExtension[path.extname(filename)];
-            if (contentType)
-                headers["Content-Type"] = contentType;
-            response.writeHead(200, headers);
-            response.write(file, "binary");
-            response.end();
-        });
-    });
-}).listen(parseInt(port, 10));
+			let headers = {}
+			let contentType = contentTypesByExtension[path.extname(filename)]
+			if (contentType)
+				headers['Content-Type'] = contentType
+			response.writeHead(200, headers)
+			response.write(file, 'binary')
+			response.end()
+		})
+	})
+}).listen(parseInt(port, 10))
 
-console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+console.log('Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown')
--- javascript/pahoclient.js
+++ javascript/pahoclient.js
@@ -3,51 +3,59 @@
 client = new Paho.MQTT.Client(
   'm20.cloudmqtt.com',
   36673,
-  'web_' + parseInt(Math.random() * 100, 10));
+  'web_' + parseInt(Math.random() * 100, 10))
 
-client.onConnectionlost = onConnectionLost;
-client.onMessageArrived = onMessageArrived;
-var options = {
-    timeout: 3,
-    useSSL: true,
-    cleanSession: true,
-    userName: '***REMOVED***',
-    password: '***REMOVED***',
-    onSuccess: onConnect,
-    onFailure: doFail,
-};
+client.onConnectionlost = onConnectionLost
+client.onMessageArrived = onMessageArrived
+let options = {
+	timeout: 3,
+	useSSL: true,
+	cleanSession: true,
+	userName: '***REMOVED***',
+	password: '***REMOVED***',
+	onSuccess: onConnect,
+	onFailure: doFail,
+}
 
 // connect the client
-client.connect(options);
+client.connect(options)
 
-// called when the client connects
+/**
+* called when the client connects
+**/
 function onConnect() {
-    console.log("onConnect");
-    client.subscribe("RF24SN/in/1/1", {qos: 0});
-    client.subscribe("RF24SN/in/1/2", {qos: 0});
+	console.log('onConnect')
+	client.subscribe('RF24SN/in/1/1', {qos: 0})
+	client.subscribe('RF24SN/in/1/2', {qos: 0})
 };
 
 function doFail(e) {
-    console.log(e);
+	console.log(e)
 };
 
-// called when the client loses its connection
+/**
+* called when the client loses its connection
+* @param {response} responseObject returns error object, print error message
+**/
 function onConnectionLost(responseObject) {
-    if (responseObject.errorCode !== 0) {
-        console.log("onConnectionLost: " + responseObject.errorMessage);
-    }
+	if (responseObject.errorCode !== 0) {
+		console.log('onConnectionLost: ' + responseObject.errorMessage)
+	}
 };
 
-// called when message arrives
+/**
+* called when message arrives
+* @param {msg} message sort depending on the message
+**/
 function onMessageArrived(message) {
-    if (message.payloadString !== "NaN") {
-        var payload = message.payloadString;
-        console.log(payload);
-        $('.loading').hide();
-        if (message.destinationName == "RF24SN/in/1/1") {
-            $('#returntemp').html(payload);
-        } else if (message.destinationName == "RF24SN/in/1/2") {
-            $('#returnhumd').html(payload);
-        }
-    }
+	if (message.payloadString !== 'NaN') {
+		let payload = message.payloadString
+		console.log(payload)
+		$('.loading').hide()
+		if (message.destinationName == 'RF24SN/in/1/1') {
+			$('#returntemp').html(payload)
+		} else if (message.destinationName == 'RF24SN/in/1/2') {
+			$('#returnhumd').html(payload)
+		}
+	}
 };
