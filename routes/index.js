36f4bae4a6ce1aecd9b585475c005e9e17e5f27e
galvinong <go1g14@soton.ac.uk>
2017-04-12 04:23:03 +0100

    added back for loop

--- routes/index.js
+++ routes/index.js
@@ -84,21 +84,21 @@ router.get('/get-data/:sensor_id/:time_created', function(req, res, next) {
 	})
 })
 
-// let channelNames = {
-// 	channel: [
-// 		{
-// 			id: 'RF24SN/in/1/1',
-// 			title: 'Temperature',
-// 			compare: '>',
-// 			warning: '20',
-// 		}, {
-// 			id: 'RF24SN/in/1/2',
-// 			title: 'Humidity',
-// 			comapre: '<',
-// 			warning: '50',
-// 		},
-// 	],
-// }
+let channelNames = {
+	channel: [
+		{
+			id: 'RF24SN/in/1/1',
+			title: 'Temperature',
+			compare: '>',
+			warning: '20',
+		}, {
+			id: 'RF24SN/in/1/2',
+			title: 'Humidity',
+			compare: '<',
+			warning: '50',
+		},
+	],
+}
 
 /**
  * Handles message arrived from broker and add to mongodb
@@ -121,30 +121,8 @@ function insertEvent(topic, payload) {
 				console.log('Error saving to mongodb')
 			}
 		})
-		console.log(topic + ' ' + payload)
-		// for (var i = 0; i < channelNames.channel.length; i++) {
-		// 	if (topic === channelNames.channel[i].id) {
-		// 		if (channelNames.channel[i].compare = '>') {
-		// 			if (payload > channelNames.channel[i].warning) {
-		// 				let message = {
-		// 					app_id: '***REMOVED***',
-		// 					contents: {'en': 'Warning: ' + channelNames.channel[i].title + ' value above range! ' + payload},
-		// 					included_segments: ['All'],
-		// 				}
-		// 				sendNotification(message)
-		// 			}
-		// 		} else if (channelNames.channel[i].compare = '<') {
-		// 			if (payload < channelNames.channel[i].warning) {
-		// 				let message = {
-		// 					app_id: '***REMOVED***',
-		// 					contents: {'en': 'Warning: ' + channelNames.channel[i].title + ' value below range! ' + payload},
-		// 					included_segments: ['All'],
-		// 				}
-		// 				sendNotification(message)
-		// 			}
-		// 		}
-		// 	}
-		// }
+		// console.log(topic + ' ' + payload)
+		checkNotification()
 		// Add onesignal code here
 
 		// if (topic === 'RF24SN/in/1/1') {
@@ -175,7 +153,29 @@ function insertEvent(topic, payload) {
  * @param  {[type]} payload [apply checking based on the payload]
  */
 function checkNotification(topic, payload) {
-
+	for (var i = 0; i < channelNames.channel.length; i++) {
+		if (topic === channelNames.channel[i].id) {
+			if (channelNames.channel[i].compare = '>') {
+				if (payload > channelNames.channel[i].warning) {
+					let message = {
+						app_id: '***REMOVED***',
+						contents: {'en': 'Warning: ' + channelNames.channel[i].title + ' value above range! ' + payload},
+						included_segments: ['All'],
+					}
+					sendNotification(message)
+				}
+			} else if (channelNames.channel[i].compare = '<') {
+				if (payload < channelNames.channel[i].warning) {
+					let message = {
+						app_id: '***REMOVED***',
+						contents: {'en': 'Warning: ' + channelNames.channel[i].title + ' value below range! ' + payload},
+						included_segments: ['All'],
+					}
+					sendNotification(message)
+				}
+			}
+		}
+	}
 }
 
 module.exports = router
